00004017f91c1c69543910339d6b709397b948e0
Unknown <florian.meyer@edu.hefr.ch>
2018-05-31 10:32:03 +0200

    async

--- code/src/controller/PixelFlowRegion.java
+++ code/src/controller/PixelFlowRegion.java
@@ -164,7 +164,7 @@ public class PixelFlowRegion {
 		}
 	}
 
-	@POPClassyncConc
+	@POPAsyncConc
     public void updateFlows(double elapsedTime) {
 
 			PixelFlowRegion me = (PixelFlowRegion) PopJava.getThis(this);
